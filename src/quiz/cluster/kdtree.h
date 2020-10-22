e6b58cee50d2f4dc2d6071d9b7a4d17b84497070
Ali <engalidanish@gmail.com>
2020-10-22 20:47:26 +0200

    Kdtree Implementation: search and insert functions

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -7,38 +7,79 @@
 // Structure to represent node of kd tree
 struct Node
 {
-	std::vector<float> point;
+	std::vector<float> point; //point[0] is x point[1] is y
 	int id;
 	Node* left;
 	Node* right;
 
-	Node(std::vector<float> arr, int setId)
-	:	point(arr), id(setId), left(NULL), right(NULL)
+	Node(const std::vector<float> arr, const int setId)
+	:	point(arr), id(setId), left(nullptr), right(nullptr)
 	{}
+	~Node()= default;
 };
 
 struct KdTree
 {
 	Node* root;
 
-	KdTree()
-	: root(NULL)
-	{}
+	KdTree() : root(nullptr){}
 
-	void insert(std::vector<float> point, int id)
+	void inserPoint(Node** root, std::vector<float>& point, uint depth, int id)
+    {
+	    if (*root == nullptr)
+        {
+            *root = new Node(point, id);
+        }
+        else
+        {
+            uint current_dim = depth % 2; //selecting x or y index for comparison as depth of the tree when it is traversed
+            if (point[current_dim] < (*root)->point[current_dim])
+                inserPoint(&((*root)->left), point, depth+1, id);
+            else
+                inserPoint(&((*root)->right), point, depth+1, id);
+        }
+    }
+
+	void insert(std::vector<float>& point, int id)
 	{
-		// TODO: Fill in this function to insert a new point into the tree
-		// the function should create a new node and place correctly with in the root 
-
+		inserPoint(&root, point, 0, id);
 	}
+
+	void searchPoint(Node* kdnode, const std::vector<float>& target, std::vector<int>& ids, uint depth, const float distanceTol)
+    {
+	    // compare tolerance of target x and y against Node x, y
+	    // compute distance if Node x,y is within target x,y with tolerances applied
+	    // if distance is less then or equal to given global tolerance push the ids of the points
+	    // all the steps are done while traversing the nodes where left nodes are traversed when
+	    // dim_point - tolerance < node_point dim and right node is traversed dim_point + tolerance > node_point dim
+	    if (kdnode != nullptr)
+	    {
+            if ((kdnode->point[0]) >= (target[0] - distanceTol) && (kdnode->point[0]) <= (target[0] + distanceTol)
+                && (kdnode->point[1]) >= (target[1] - distanceTol) && (kdnode->point[1]) <= (target[1] + distanceTol))
+            {
+                float target_node_distance = sqrt((kdnode->point[0] - target[0]) * (kdnode->point[0] - target[0]) +
+                                                  (kdnode->point[1] - target[1]) * (kdnode->point[1] - target[1]));
+                if (target_node_distance < distanceTol)
+                    ids.push_back(kdnode->id);
+            }
+
+            uint current_dim = depth % 2;
+            if (target[current_dim] - distanceTol < kdnode->point[current_dim])
+                searchPoint(kdnode->left, target, ids, depth + 1, distanceTol);
+            if (target[current_dim] + distanceTol > kdnode->point[current_dim])
+                searchPoint(kdnode->right, target, ids, depth + 1, distanceTol);
+        }
+    }
 
 	// return a list of point ids in the tree that are within distance of target
-	std::vector<int> search(std::vector<float> target, float distanceTol)
+	std::vector<int> search(const std::vector<float>& target, const float distanceTol)
 	{
-		std::vector<int> ids;
-		return ids;
+		std::vector<int> ids_vector;
+		searchPoint(root, target, ids_vector, 0, distanceTol);
+		return ids_vector;
 	}
-	
+
+	~KdTree()= default;
 
 };
 
